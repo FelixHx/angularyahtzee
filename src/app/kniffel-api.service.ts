eb5fa42c2e102acc7a5ebf249e671308ce1e5d58
Felix Holderied <felix@holderied.de>
2024-10-11 15:25:01 +0200

    format

--- src/app/kniffel-api.service.ts
+++ src/app/kniffel-api.service.ts
@@ -7,62 +7,62 @@ import { NgFor, NgIf } from '@angular/common';
 })
 
 export class KniffelApiService {
-  fields : Row[] = [];
+  fields: Row[] = [];
 
   init() {
-    this.fields  = [
-      {id: "1", title:"Aces", points: null, sum:false},
-      {id: "2", title:"Twos", points: null, sum:false},
-      {id: "3", title:"Threes", points: null, sum:false},
-      {id: "4", title:"Fours", points: null, sum:false},
-      {id: "5", title:"Fives", points: null, sum:false},
-      {id: "6", title:"Sixes", points: null, sum:false},
-      {id: "T", title:"Total", points: null, sum:true},
-      {id: "B", title:"Bonus", points: null, sum:true},
-      {id: "TU", title:"Total Upper", points: null, sum:true},
-      {id: "3K", title:"Three of a Kind", points: null, sum:false},
-      {id: "4K", title:"Four of a Kind", points: null, sum:false},
-      {id: "FH", title:"Full House", points: null, sum:false},
-      {id: "SS", title:"Small Straight", points: null, sum:false},
-      {id: "LS", title:"Large Straight", points: null, sum:false},
-      {id: "Y", title:"Yahtzee", points: null, sum:false},
-      {id: "CH", title:"Chance", points: null, sum:false},
-      {id: "TL", title:"Total Lower", points: null, sum:true},
-      {id: "TU", title:"Total Upper", points: null, sum:true},
-      {id: "GT", title:"Grand Total", points: null, sum:true}]
+    this.fields = [
+      { id: "1", title: "Aces", points: null, sum: false },
+      { id: "2", title: "Twos", points: null, sum: false },
+      { id: "3", title: "Threes", points: null, sum: false },
+      { id: "4", title: "Fours", points: null, sum: false },
+      { id: "5", title: "Fives", points: null, sum: false },
+      { id: "6", title: "Sixes", points: null, sum: false },
+      { id: "T", title: "Total", points: null, sum: true },
+      { id: "B", title: "Bonus", points: null, sum: true },
+      { id: "TU", title: "Total Upper", points: null, sum: true },
+      { id: "3K", title: "Three of a Kind", points: null, sum: false },
+      { id: "4K", title: "Four of a Kind", points: null, sum: false },
+      { id: "FH", title: "Full House", points: null, sum: false },
+      { id: "SS", title: "Small Straight", points: null, sum: false },
+      { id: "LS", title: "Large Straight", points: null, sum: false },
+      { id: "Y", title: "Yahtzee", points: null, sum: false },
+      { id: "CH", title: "Chance", points: null, sum: false },
+      { id: "TL", title: "Total Lower", points: null, sum: true },
+      { id: "TU", title: "Total Upper", points: null, sum: true },
+      { id: "GT", title: "Grand Total", points: null, sum: true }]
   }
 
-  getAll()  : Row[] {
-    var pointsT:number = 0;
-    var pointsB:number = 0;
-    var pointsTU:number = 0;
-    var pointsTL:number = 0;
-    var pointsGT:number = 0;
+  getAll(): Row[] {
+    var pointsT: number = 0;
+    var pointsB: number = 0;
+    var pointsTU: number = 0;
+    var pointsTL: number = 0;
+    var pointsGT: number = 0;
 
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '1' )?.points || 0);
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '2' )?.points || 0);
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '3' )?.points || 0);
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '4' )?.points || 0);
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '5' )?.points || 0);
-    pointsT = pointsT+ (this.fields.find( ({ id }) => id === '6' )?.points || 0);
-    if (pointsT>=63) pointsB=35;
-    pointsTU = pointsT+pointsB;
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === '3K' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === '4K' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === 'FH' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === 'SS' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === 'LS' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === 'Y' )?.points || 0);
-    pointsTL = pointsTL+ (this.fields.find( ({ id }) => id === 'CH' )?.points || 0);
-    pointsGT = pointsTU+pointsTL;
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '1')?.points || 0);
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '2')?.points || 0);
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '3')?.points || 0);
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '4')?.points || 0);
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '5')?.points || 0);
+    pointsT = pointsT + (this.fields.find(({ id }) => id === '6')?.points || 0);
+    if (pointsT >= 63) pointsB = 35;
+    pointsTU = pointsT + pointsB;
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === '3K')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === '4K')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === 'FH')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === 'SS')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === 'LS')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === 'Y')?.points || 0);
+    pointsTL = pointsTL + (this.fields.find(({ id }) => id === 'CH')?.points || 0);
+    pointsGT = pointsTU + pointsTL;
 
     //console.log(pointsTU);
     for (var val of this.fields) {
-      if (val.id == "T" ) val.points=pointsT; 
-      if (val.id == "B" ) val.points=pointsB; 
-      if (val.id == "TU" ) val.points=pointsTU; 
-      if (val.id == "TL" ) val.points=pointsTL; 
-      if (val.id == "GT" ) val.points=pointsGT; 
+      if (val.id == "T") val.points = pointsT;
+      if (val.id == "B") val.points = pointsB;
+      if (val.id == "TU") val.points = pointsTU;
+      if (val.id == "TL") val.points = pointsTL;
+      if (val.id == "GT") val.points = pointsGT;
     }
     return this.fields;
   }
--- src/app/kniffel/kniffel.component.ts
+++ src/app/kniffel/kniffel.component.ts
@@ -13,70 +13,68 @@ import { KniffelApiService } from '../kniffel-api.service';
 })
 export class KniffelComponent {
 
-  fields : Row[] =[];
-  
-  constructor(private kniffelApiService : KniffelApiService) {
+  fields: Row[] = [];
+
+  constructor(private kniffelApiService: KniffelApiService) {
     this.kniffelApiService.init();
     this.fields = this.kniffelApiService.getAll();
   };
 
 
-  points(fieldId:string, dices:Dice[]) : number{
+  points(fieldId: string, dices: Dice[]): number {
     var sum = 0;
-    var histogramm = [0,0,0,0,0,0];
-       
-    dices.forEach(function (value){
-      histogramm[value.val-1] ++;
+    var histogramm = [0, 0, 0, 0, 0, 0];
+
+    dices.forEach(function (value) {
+      histogramm[value.val - 1]++;
     })
 
-    var maxHist=Math.max.apply(null, histogramm);
+    var maxHist = Math.max.apply(null, histogramm);
 
-    dices.forEach(function (value){
-      if (fieldId=="1" && value.val==1) sum += value.val;
-      if (fieldId=="2" && value.val==2) sum += value.val;
-      if (fieldId=="3" && value.val==3) sum += value.val;
-      if (fieldId=="4" && value.val==4) sum += value.val;
-      if (fieldId=="5" && value.val==5) sum += value.val;
-      if (fieldId=="6" && value.val==6) sum += value.val;
-      if (fieldId=="3K" && maxHist>=3) sum += value.val;
-      if (fieldId=="4K" && maxHist>=4) sum += value.val;
-      if (fieldId=="FH" && maxHist>=3 && (histogramm.includes(3)&& histogramm.includes(2)||maxHist==5)) sum = 25;
-      if (fieldId=="SS" && 
+    dices.forEach(function (value) {
+      if (fieldId == "1" && value.val == 1) sum += value.val;
+      if (fieldId == "2" && value.val == 2) sum += value.val;
+      if (fieldId == "3" && value.val == 3) sum += value.val;
+      if (fieldId == "4" && value.val == 4) sum += value.val;
+      if (fieldId == "5" && value.val == 5) sum += value.val;
+      if (fieldId == "6" && value.val == 6) sum += value.val;
+      if (fieldId == "3K" && maxHist >= 3) sum += value.val;
+      if (fieldId == "4K" && maxHist >= 4) sum += value.val;
+      if (fieldId == "FH" && maxHist >= 3 && (histogramm.includes(3) && histogramm.includes(2) || maxHist == 5)) sum = 25;
+      if (fieldId == "SS" &&
         (
-          (histogramm[0]>0 && histogramm[1]>0 && histogramm[2]>0 && histogramm[3]>0) ||
-          (histogramm[1]>0 && histogramm[2]>0 && histogramm[3]>0 && histogramm[4]>0) ||
-          (histogramm[2]>0 && histogramm[3]>0 && histogramm[4]>0 && histogramm[5]>0)
+          (histogramm[0] > 0 && histogramm[1] > 0 && histogramm[2] > 0 && histogramm[3] > 0) ||
+          (histogramm[1] > 0 && histogramm[2] > 0 && histogramm[3] > 0 && histogramm[4] > 0) ||
+          (histogramm[2] > 0 && histogramm[3] > 0 && histogramm[4] > 0 && histogramm[5] > 0)
         )) sum = 30;
-        if (fieldId=="LS" && maxHist==1 && histogramm[0]+histogramm[5]==1) sum = 40;
-        if (fieldId=="Y" && maxHist==5) sum = 50;
-      if (fieldId=="CH") sum += value.val;
+      if (fieldId == "LS" && maxHist == 1 && histogramm[0] + histogramm[5] == 1) sum = 40;
+      if (fieldId == "Y" && maxHist == 5) sum = 50;
+      if (fieldId == "CH") sum += value.val;
     })
     return sum;
   }
 
-  rollNumber : number = 0;
+  rollNumber: number = 0;
 
-  dices : Dice[] = [
-    {val: 0, fixed: false},
-    {val: 0, fixed: false},
-    {val: 0, fixed: false},
-    {val: 0, fixed: false},
-    {val: 0, fixed: false}
+  dices: Dice[] = [
+    { val: 0, fixed: false },
+    { val: 0, fixed: false },
+    { val: 0, fixed: false },
+    { val: 0, fixed: false },
+    { val: 0, fixed: false }
   ];
 
-  switchFixed(i: number):void {
+  switchFixed(i: number): void {
     this.dices[i].fixed = !this.dices[i].fixed;
     //console.log(i);
   }
 
-  rollDices():void {
+  rollDices(): void {
     this.kniffelApiService.getAll();
-    //console.log("Roll Dice Fallera");
-    //console.log(Math.floor((Math.random() * 6) + 1));
-    this.dices.forEach(function (value){
+    if (this.rollNumber == 0) this.dices.forEach(function (value) { value.fixed = false; })
+    this.dices.forEach(function (value) {
       if (!value.fixed) value.val = Math.floor((Math.random() * 6) + 1);
     })
-    if (this.rollNumber==0) this.dices.forEach(function (value){value.fixed=false;})
     this.rollNumber++;
   }
 };
