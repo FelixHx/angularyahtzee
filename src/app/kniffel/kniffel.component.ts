41144f9a84b6907d074d8812c8ca3e5e5698772e
Felix Holderied <felix@holderied.de>
2024-10-11 12:30:13 +0200

    Alle Bewertungen, nicht schön aber funzt

--- src/app/kniffel/kniffel.component.ts
+++ src/app/kniffel/kniffel.component.ts
@@ -19,8 +19,22 @@ export class KniffelComponent {
     this.fields = this.kniffelApiService.getAll();
   };
 
+
   points(fieldId:string, dices:number[]) : number{
     var sum = 0;
+    var histogramm = [0,0,0,0,0,0];
+       
+    dices.forEach(function (value){
+      histogramm[value-1] ++;
+    })
+
+    var maxHist=Math.max.apply(null, histogramm);
+    var minHist=Math.min.apply(null, histogramm);
+
+    //histogramm.forEach( function (value){
+    //  console.log(value);
+    //})
+
     dices.forEach(function (value){
       if (fieldId=="1" && value==1) sum += value;
       if (fieldId=="2" && value==2) sum += value;
@@ -28,11 +42,23 @@ export class KniffelComponent {
       if (fieldId=="4" && value==4) sum += value;
       if (fieldId=="5" && value==5) sum += value;
       if (fieldId=="6" && value==6) sum += value;
+      if (fieldId=="3K" && maxHist>=3) sum += value;
+      if (fieldId=="4K" && maxHist>=4) sum += value;
+      if (fieldId=="FH" && maxHist>=3 && (histogramm.includes(3)&& histogramm.includes(2)||maxHist==5)) sum += value;
+      if (fieldId=="SS" && 
+        (
+          (histogramm[0]>0 && histogramm[1]>0 && histogramm[2]>0 && histogramm[3]>0) ||
+          (histogramm[1]>0 && histogramm[2]>0 && histogramm[3]>0 && histogramm[4]>0) ||
+          (histogramm[2]>0 && histogramm[3]>0 && histogramm[4]>0 && histogramm[5]>0)
+        )) sum = 30;
+        if (fieldId=="LS" && maxHist==1 && histogramm[0]+histogramm[5]==1) sum = 40;
+        if (fieldId=="Y" && maxHist==5) sum = 50;
+      if (fieldId=="CH") sum += value;      
     })
     return sum;
   }
 
-  dices : number[] = [5,5,6,2,1];
+  dices : number[] = [5,2,6,3,4];
 
   rollDice():void {
     this.kniffelApiService.getAll();
@@ -45,9 +71,7 @@ export class KniffelComponent {
       Math.floor((Math.random() * 6) + 1),
       Math.floor((Math.random() * 6) + 1),
     ];
-    console.log(this.points("6", this.dices));
+    //console.log(this.points("6", this.dices));
   };
-
-
 };
 
