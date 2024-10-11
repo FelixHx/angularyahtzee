250a2b56063a21b40a365804f65444a4c95547c2
Felix Holderied <felix@holderied.de>
2024-10-11 14:12:32 +0200

    refactored

--- src/app/kniffel/kniffel.component.ts
+++ src/app/kniffel/kniffel.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { Row } from '../kniffel/row';
+import { Dice } from '../kniffel/dice';
 import { KniffelApiService } from '../kniffel-api.service';
 
 @Component({
@@ -20,31 +21,26 @@ export class KniffelComponent {
   };
 
 
-  points(fieldId:string, dices:number[]) : number{
+  points(fieldId:string, dices:Dice[]) : number{
     var sum = 0;
     var histogramm = [0,0,0,0,0,0];
        
     dices.forEach(function (value){
-      histogramm[value-1] ++;
+      histogramm[value.val-1] ++;
     })
 
     var maxHist=Math.max.apply(null, histogramm);
-    var minHist=Math.min.apply(null, histogramm);
-
-    //histogramm.forEach( function (value){
-    //  console.log(value);
-    //})
 
     dices.forEach(function (value){
-      if (fieldId=="1" && value==1) sum += value;
-      if (fieldId=="2" && value==2) sum += value;
-      if (fieldId=="3" && value==3) sum += value;
-      if (fieldId=="4" && value==4) sum += value;
-      if (fieldId=="5" && value==5) sum += value;
-      if (fieldId=="6" && value==6) sum += value;
-      if (fieldId=="3K" && maxHist>=3) sum += value;
-      if (fieldId=="4K" && maxHist>=4) sum += value;
-      if (fieldId=="FH" && maxHist>=3 && (histogramm.includes(3)&& histogramm.includes(2)||maxHist==5)) sum += value;
+      if (fieldId=="1" && value.val==1) sum += value.val;
+      if (fieldId=="2" && value.val==2) sum += value.val;
+      if (fieldId=="3" && value.val==3) sum += value.val;
+      if (fieldId=="4" && value.val==4) sum += value.val;
+      if (fieldId=="5" && value.val==5) sum += value.val;
+      if (fieldId=="6" && value.val==6) sum += value.val;
+      if (fieldId=="3K" && maxHist>=3) sum += value.val;
+      if (fieldId=="4K" && maxHist>=4) sum += value.val;
+      if (fieldId=="FH" && maxHist>=3 && (histogramm.includes(3)&& histogramm.includes(2)||maxHist==5)) sum = 25;
       if (fieldId=="SS" && 
         (
           (histogramm[0]>0 && histogramm[1]>0 && histogramm[2]>0 && histogramm[3]>0) ||
@@ -53,25 +49,26 @@ export class KniffelComponent {
         )) sum = 30;
         if (fieldId=="LS" && maxHist==1 && histogramm[0]+histogramm[5]==1) sum = 40;
         if (fieldId=="Y" && maxHist==5) sum = 50;
-      if (fieldId=="CH") sum += value;      
+      if (fieldId=="CH") sum += value.val;
     })
     return sum;
   }
 
-  dices : number[] = [5,2,6,3,4];
+  dices : Dice[] = [
+    {val: 0, fixed: false},
+    {val: 0, fixed: false},
+    {val: 0, fixed: false},
+    {val: 0, fixed: false},
+    {val: 0, fixed: false}
+  ];
 
   rollDice():void {
     this.kniffelApiService.getAll();
     //console.log("Roll Dice Fallera");
     //console.log(Math.floor((Math.random() * 6) + 1));
-    this.dices = [
-      Math.floor((Math.random() * 6) + 1),
-      Math.floor((Math.random() * 6) + 1),
-      Math.floor((Math.random() * 6) + 1),
-      Math.floor((Math.random() * 6) + 1),
-      Math.floor((Math.random() * 6) + 1),
-    ];
-    //console.log(this.points("6", this.dices));
-  };
+    this.dices.forEach(function (value){
+      value.val = Math.floor((Math.random() * 6) + 1);
+    })
+  }
 };
 
